c4ef4522bc5347c31242ecb3101b3d856ba54cd7
spickfeet <slavaspickfeet@mail.ru>
2024-03-09 17:39:10 +0300

    Добавьте файлы проекта.

---
+++ ComputationalMathConsole/Matrix.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComputationalMathConsole
+{
+    public class Matrix
+    {
+        private int _height;
+        private int _width;
+        private float[,] _numbers;
+
+        public int Height
+        {
+            get { return _height; }
+            set { _height = value; }
+        }
+
+        public int Width
+        {
+            get { return _width; }
+            set { _width = value; }
+        }
+        public float[,] Numbers
+        {
+            get { return _numbers; }
+            set
+            {
+                for (int i = 0; i < value.GetLength(0); i++)
+                {
+                    for (int j = 0; j < value.GetLength(1); j++)
+                    {
+                        _numbers[i, j] = value[i, j];
+                    }
+                }
+            }
+        }
+
+        public Matrix(float[,] numbers)
+        {
+            Height = numbers.GetLength(0);
+            Width = numbers.GetLength(1);
+            _numbers = new float[_height, _width];
+            Numbers = numbers;
+        }
+        public float this[int i, int j]
+        {
+            get => Numbers[i, j];
+            set => Numbers[i, j] = value;
+        }
+        public void Print()
+        {
+            for (int i = 0; i < _height; i++)
+            {
+                for (int j = 0; j < _width; j++)
+                {
+                    Console.Write($"{_numbers[i, j]} ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+        //public static Matrix operator +(Matrix m1, Matrix m2)
+        //{
+        //    if (m1._height != m2._height || m1._width != m2._width)
+        //    {
+        //        throw new ArgumentException("Нельзя складывать матрицы разного размера");
+        //    }
+        //    for (int i = 0; i < m1._height; i++)
+        //    {
+        //        for (int j = 0; j < m1._width; j++)
+        //        {
+        //            m1[i, j] += m2[i, j];
+        //        }
+        //    }
+        //    return m1;
+        //}
+        //public static Matrix operator *(Matrix m1, Matrix m2)
+        //{
+        //    Matrix result = new Matrix(m1._height, m1._width);
+        //    for (int i = 0; i < m1._height; i++)
+        //    {
+        //        for (int j = 0; j < m1._width; j++)
+        //        {
+
+        //        }
+        //    }
+        //    return m1;
+        //}
+    }
+}
---
+++ ComputationalMathConsole/Program.cs
@@ -0,0 +1,105 @@
+﻿namespace ComputationalMathConsole;
+
+
+class GausMethod
+{
+    private Matrix _matrix;
+    public Matrix Matrix
+    {
+        get { return _matrix; }
+        set { _matrix = value; }
+    }
+    public GausMethod(Matrix matrix)
+    {
+        Matrix = matrix;
+    }
+    public void Direct()
+    {
+        int n = 0;
+        for (int i = 0; i < Matrix.Height - 1; i++)
+        {
+            for (int a = n + 1; a < Matrix.Height; a++)
+            {
+                float coef = Matrix[a, i] / Matrix[n, i];
+
+                for (int w = 0; w < Matrix.Width; w++)
+                {
+                    Matrix[a, w] -= Matrix[n, w] * coef;
+                }
+            }
+            Matrix.Print();
+            if (n < Matrix.Height)
+            {
+                n++;
+            }
+            else break;
+        }
+    }
+    public void Indirect()
+    {
+        int n = Matrix.Height - 1;
+        for (int i = Matrix.Width - 2; i > -1; i--)
+        {
+            for (int a = n - 1; a > -1; a--)
+            {
+                float coef = Matrix[a, i] / Matrix[n, i];
+                for (int w = Matrix.Width - 1; w > -1; w--)
+                {
+                    Matrix[a, w] -= Matrix[n, w] * coef;
+                }
+            }
+            if (n > 1)
+            {
+                n--;
+            }
+            else break;
+        }
+    }
+
+    public float[] GiveAnswers()
+    {
+        float[] answers = new float[Matrix.Height];
+        for (int i = 0; i < Matrix.Height; i++)
+        {
+            float coef = 0;
+            for (int j = 0; j < Matrix.Width - 1; j++)
+            {
+                if (Matrix[i, j] != 0)
+                {
+                    coef = Matrix[i, j];
+                }
+            }
+
+            answers[i] = Matrix[i, Matrix.Width - 1] / coef;
+        }
+
+        return answers;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Matrix m1 = new(new float[3, 4] { { 5, 2.833333f, 1, 11.666666f }, { 3, 2.833333f, 7, 11.666666f }, { 1, 8, 1, 18 } });
+
+        //m1.Print();
+        //GausMethod g = new GausMethod();
+        //g.GetMatrix(m1);
+        //g.Direct();
+        //m1 = g.matrix;
+        //g.Indirect();
+        ////m1 = g.matrix;
+        //m1.Print();
+        //Console.WriteLine();
+        //Console.WriteLine();
+        //Console.WriteLine("Ответы: ");
+
+        //float[] answers = g.GiveAnswers();
+        //foreach (var ans in answers)
+        //{
+        //    Console.Write($"{ans} ");
+        //}
+        ////Console.WriteLine();
+    }
+}
