4c67fc2076c962f5a18d46a838778c68247aa9da
spickfeet <slavaspickfeet@mail.ru>
2024-03-14 13:10:52 +0300

    Интерполяционный многочлен в форме Лагранжа

--- InterpolatingLagrange/Lagrange.cs
+++ InterpolatingLagrange/Lagrange.cs
@@ -8,20 +8,28 @@ namespace InterpolatingLagrange
 {
     public class Lagrange
     {
-        public double Interpolate(double[][] numbers, double x)
+        public double Interpolate(double[,] numbers, double x)
         {
-            double result =0;
+            double result = 0;
+            double[] l = new double[numbers.GetLength(1)];
+            double li = 1;
 
-            for(int i = 0;i < numbers.Length; i++)
+            for(int i = 0;i < numbers.GetLength(1); i++)
             {
-                for (int j = 0; j < numbers.Length; j++)
+                for (int j = 0; j < numbers.GetLength(1); j++)
                 {
-                    if (i == j)
+                    if (i != j)
                     {
-                        result = (x - numbers[0][j])/(numbers[0][i] - numbers[0][j])* numbers[1][i];
-
+                        li *= (x - numbers[0,j]) / (numbers[0, i] - numbers[0, j]);
                     }
                 }
+                li *= numbers[1, i];
+                l[i] = li;
+                li = 1;
+            }
+            foreach(int i in l)
+            {
+                result += i;
             }
             return result;
         }
--- InterpolatingLagrange/Program.cs
+++ InterpolatingLagrange/Program.cs
@@ -1,1 +1,11 @@
-﻿
+﻿namespace InterpolatingLagrange;
+public class Program
+{
+    static void Main()
+    {
+        Lagrange l = new();
+        double[,] numbers = new double[,] { { 2, 3, 5 }, { 6, 4, -6 } };
+        Console.WriteLine(l.Interpolate(numbers, 5));
+    }
+}
+
