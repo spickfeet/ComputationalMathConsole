1134da7156b8143a52f4b5ba91b2bd214fb9415c
spickfeet <slavaspickfeet@mail.ru>
2024-03-14 10:44:39 +0300

    Лагранж

---
+++ InterpolatingLagrange/Lagrange.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterpolatingLagrange
+{
+    public class Lagrange
+    {
+        public double Interpolate(double[][] numbers, double x)
+        {
+            double result =0;
+
+            for(int i = 0;i < numbers.Length; i++)
+            {
+                for (int j = 0; j < numbers.Length; j++)
+                {
+                    if (i == j)
+                    {
+                        result = (x - numbers[0][j])/(numbers[0][i] - numbers[0][j])* numbers[1][i];
+
+                    }
+                }
+            }
+            return result;
+        }
+    }
+}
